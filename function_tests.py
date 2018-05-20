366aeff37c5c5f2574286fa98c2c7e5811b6fe54
rice <riceflower@foxmail.com>
2018-05-20 22:04:33 +0800

    GUI测试中新增了对输入框、一级标题的测试

--- function_tests.py
+++ function_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,8 +15,33 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
+		#访问首页
 		self.browser.get('http://localhost:8000')
+
+		#网页标题和头部都包含“To-Do”
 		self.assertIn('To-Do',self.browser.title)
+		header_text = self.browser.find_elements_by_tag_name('h1').text
+		self.assertIn('To-Do',header_text)
+		
+		#应用请他输入一个待办事项
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		#在文本框输入“Buy peacock feathers”
+		inputbox.send_keys('Buy peacock feathers')
+		#按回车后页面刷新，待办事项表格中显示了“1：Buy peacock feathers”
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1：Buy peacock feathers' for row in rows)
+		)
+
+		#页面又显示了一个文本框，可以输入其他待办事项，他输入“Use peacock feathers to make a fly"
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
